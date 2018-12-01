c6348b942f27cf2462aef26ca9a556b85b457e6b
Josh Ethridge <joshua.ethridge13@gmail.com>
2018-12-01 16:30:06 -0500

    Day 1 solved!

--- main.test.ts
+++ main.test.ts
@@ -1,7 +1,19 @@
-import Test from './main'
+import { Test } from './main'
+import { Day1 } from './main'
 
 describe('Basic tests', () => {
 	it('Module is able to export basic class function', () => {
 		expect(Test.helloWorld()).toEqual('Hello, World!')
 	})
-})
+});
+
+describe('Day 1', () => {
+	const day1 = new Day1();
+	it('Part 1 answer equals 578', () => {
+		expect(day1.part1()).toEqual(578)
+	})
+
+	it('Part 2 answer equals 82516', () => {
+		expect(day1.part2()).toEqual(82516)
+	})
+});
--- main.ts
+++ main.ts
@@ -1,6 +1,58 @@
-export default class Test {
+import fs = require('fs');
+
+class Test {
 
 	static helloWorld(): string {
 		return 'Hello, World!'
 	}
+}
+
+class Helper {
+
+	loadFileSync(path: string): string {
+		return fs.readFileSync(path, 'utf8');
+	}
+
+	parseIntWithSign(str: string): number {
+		return Number(str);
+	}
+}
+
+class Day1 {
+
+	private helper = new Helper();
+
+	part1(): number {
+		// 578
+		let frequency = 0;
+		const data = this.helper.loadFileSync('Day1.txt');
+		const lines = data.split('\n');
+		for (const line of lines) {
+			frequency += this.helper.parseIntWithSign(line);
+		}
+		return frequency;
+	}
+
+	part2(): number {
+		// 82516
+		let frequency = 0;
+		const data = this.helper.loadFileSync('Day1.txt');
+		const lines = data.split('\n');
+		const frequencyMap = new Map<number, number>();
+		for (const line of lines) {
+			frequencyMap.set(frequency, 1);
+			frequency += this.helper.parseIntWithSign(line);
+			if (frequencyMap.has(frequency)) {
+				return frequency;
+			}
+			lines.push(line);
+		}
+		return frequency;
+	}
+}
+
+export {
+	Test,
+	Helper,
+	Day1
 }
